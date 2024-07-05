0ecac637b1289ae687a6e62e5b172515149c61e6
Aleksandr Arsentev <arsenntv@gmail.com>
2024-07-05 18:39:24 -0400

    Movie Details page implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import StarRating from "./StarRating";
 
 // Temporary Data
 const tempMovieData = [
@@ -207,12 +208,85 @@ function ErrorMessage({ message }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating: imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+    Country: country,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const url = `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`;
+          const res = await fetch(url);
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies!");
+          const data = await res.json();
+          console.log(data);
+          setMovie(data);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
   return (
-    <div className="detail">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+    <div className="details">
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+
+      {!isLoading && !error && (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${title}`} />
+            <div className="details-overview">
+              <h2> {title} </h2>
+              <p> {country} </p>
+              <p>
+                {released} &bull; {year}
+              </p>
+              <p> {genre} </p>
+              <p>
+                <span> ⭐️ </span> {imdbRating} IMDb Rating{" "}
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+
+            <p>
+              <em> {plot} </em>
+            </p>
+            <p> Starring {actors} </p>
+            <p> Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
