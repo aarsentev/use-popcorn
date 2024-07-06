956beb110ed22c1e42543cc0c10637ac1660dbe2
Aleksandr Arsentev <arsenntv@gmail.com>
2024-07-05 22:45:58 -0400

    Movie components separated

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react";
+import Loader from "./Loader";
+import ErrorMessage from "./ErrorMessage";
+import StarRating from "./StarRating";
+import { KEY } from "../App";
+
+export default function MovieDetails({
+  selectedId,
+  onCloseMovie,
+  onAddWatched,
+  watched,
+}) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [userRating, setUserRating] = useState("");
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId
+  )?.userRating;
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+    Country: country,
+  } = movie;
+
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbID: selectedId,
+      title,
+      year,
+      poster,
+      imdbRating: Number(imdbRating),
+      runtime: Number(runtime.split(" ").at(0)),
+      userRating,
+    };
+
+    onAddWatched(newWatchedMovie);
+    onCloseMovie();
+  }
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const url = `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`;
+          const res = await fetch(url);
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies!");
+          const data = await res.json();
+          console.log(data);
+          setMovie(data);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
+  return (
+    <div className="details">
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+
+      {!isLoading && !error && (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${title}`} />
+            <div className="details-overview">
+              <h2> {title} </h2>
+              <p> {country} </p>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p> {genre} </p>
+              <p>
+                <span> ⭐️ </span> {imdbRating} IMDb Rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              {!isWatched ? (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRating={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      + Add to list
+                    </button>
+                  )}
+                </>
+              ) : (
+                <p> You rated this movie with {watchedUserRating} ⭐️ </p>
+              )}
+            </div>
+
+            <p>
+              <em> {plot} </em>
+            </p>
+            <p> Starring {actors} </p>
+            <p> Directed by {director}</p>
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
