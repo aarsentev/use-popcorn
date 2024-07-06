7439e15065d0719313e7743f534418b8453f3ae8
Aleksandr Arsentev <arsenntv@gmail.com>
2024-07-05 22:47:01 -0400

    Main component refactored

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={10} color="red" size={24} default={3} /> */}
   </React.StrictMode>
 );
